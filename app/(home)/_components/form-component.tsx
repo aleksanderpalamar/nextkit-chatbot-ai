22e2c0d8b9594cccaedc99223bd3e3373967b1b0
aleksanderpalamar <apalamar@live.com>
2024-07-23 10:50:42 -0300

    update

--- app/(home)/_components/form-component.tsx
+++ app/(home)/_components/form-component.tsx
@@ -2,13 +2,13 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
 import { useChat } from "ai/react";
-import { Bot, SendIcon, User2 } from "lucide-react";
+import { SendIcon, User2 } from "lucide-react";
 import { useState, useEffect, useRef } from "react";
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import Image from "next/image";
+import { Label } from "@/components/ui/label";
 
 export const FormComponent = () => {
   const { input, handleInputChange, handleSubmit, messages } = useChat();
@@ -27,12 +27,11 @@ export const FormComponent = () => {
     }
   };
 
-  // Use useEffect to scroll to the bottom when messages change
   useEffect(() => {
     if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: "auto" });
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }, [messages]);
+  }, []);
 
   return (
     <>
@@ -46,7 +45,7 @@ export const FormComponent = () => {
                     <AvatarImage src="/placeholder-user.jpg" />
                     <AvatarFallback>
                       <div className="w-8 h-8 rounded-full bg-violet-500 flex items-center justify-center">
-                        <User2 className="w-6 h-6 text-white"/>
+                        <User2 className="w-6 h-6 text-white" />
                       </div>
                     </AvatarFallback>
                   </Avatar>
@@ -79,31 +78,33 @@ export const FormComponent = () => {
             )}
           </div>
         ))}
-        {/* Empty div used to scroll into view */}
+        <div className="clear-both" />
         <div ref={messagesEndRef} />
       </div>
       <div className="bg-violet-200 p-4 flex items-center border-t border-violet-300">
-        <Textarea
-          value={input}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-          placeholder="Type your message here 'Press enter to send'"
-          className="flex-1 rounded-md border-none border-input bg-violet-100
-           px-4 py-2 text-foreground placeholder:text-zinc-400 
-           focus:outline-none focus:ring-2 focus:ring-violet-400 resize-none"
-        />
-        <Button
-          variant="default"
-          size="icon"
-          onClick={handleSubmit}
-          type="submit"
-          className="ml-4 rounded-md bg-violet-500 text-white 
+        <Label className="w-full flex items-center gap-2 bg-violet-400 rounded-md p-2 
+        focus-within:ring-2 focus-within:ring-violet-400 focus-within:ring-offset-2 focus-within:ring-offset-violet-50">
+          <textarea
+            value={input}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            placeholder="Type your message here 'Press enter to send'"
+            className="flex-1 rounded-md border-none border-input bg-transparent 
+            resize-none focus:ring-0 outline-none placeholder:text-zinc-300 text-zinc-100"
+          />
+          <Button
+            variant="default"
+            size="icon"
+            onClick={handleSubmit}
+            type="submit"
+            className="ml-4 rounded-md bg-violet-500 text-white 
           hover:bg-violet-500/80 flex items-center justify-center 
           disabled:cursor-not-allowed disabled:opacity-50"
-        >
-          <SendIcon className="w-5 h-5" />
-          <span className="sr-only">Enviar</span>
-        </Button>
+          >
+            <SendIcon className="w-5 h-5" />
+            <span className="sr-only">Enviar</span>
+          </Button>
+        </Label>
       </div>
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex-1 max-w-6xl mx-auto">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
